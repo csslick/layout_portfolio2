a7a7e226364955e9cd999658b22afa51841b149e
csslick <digitone@naver.com>
2018-08-30 17:04:13 +0900

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -27,21 +27,49 @@ var pie_data4 = {
 	lineCap: 'butt' ,
 	size: 140
 	}
-                
+
+// 차트 업데이트(차트위치요 소명)
+function update_pieChart(el){
+	var scroll_top = $(window).scrollTop();
+	console.log(scroll_top);
+	if(scroll_top >= el){
+		var chart_data = [];
+		chart_data.push($('.chart1').attr('data-percent'));
+		chart_data.push($('.chart2').attr('data-percent'));
+		chart_data.push($('.chart3').attr('data-percent'));
+		chart_data.push($('.chart4').attr('data-percent'));
+		console.log(chart_data);
+		$('[class^=chart]').each(function(){
+			var val = $(this).attr('data-percent');
+			$(this).data('easyPieChart').update(0).update(val);	
+		});
+	}	
+}                
+
+
 $(function() {
+	
+	var about_top = $('#about').offset().top;
+	console.log(about_top);
+	
+	$(window).on('scroll', function(){
+		
+		// 파이차트 업데이트(#차트위치 요소명)
+		update_pieChart(about_top);
+	});
+	
     // 파이차트 세팅
 	$('.chart1').easyPieChart(pie_data1);
 	$('.chart2').easyPieChart(pie_data2); 
 	$('.chart3').easyPieChart(pie_data3);     
 	$('.chart4').easyPieChart(pie_data4);     
 	
-	// 파이차트내 정보 표시(data-percent)
+	// 파이차트 내 정보 표시(data-percent)
 	$('[class^=chart]').each(function(i){
 		var attr = $(this).attr('data-percent');
 		$(this).find('span').text(attr);
 	})
 	
-	$('.btn').click(function(){
-		$('.chart1').data('easyPieChart').update(0).update(50);
-	});	
+
+	
 });
